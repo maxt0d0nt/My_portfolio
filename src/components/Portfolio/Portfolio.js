36e9a1a0c218b51216a6e95d19b2e08b18d533a2
maxt0d0nt <maxbox21@hotmail.com>
2022-08-23 18:54:30 +0200

    nuevo proyecto

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState} from 'react';
 import './Portfolio.scss';
 import { Animated } from '../AnimatedLetters/Animated';
 import CRS from '../../assets/images/crs front.png';
+import IPC from '../../assets/images/ipcweb.jpg';
 
 
 
@@ -28,10 +29,23 @@ useEffect (() => {
 
     < div className='container-work'>
       <div className='webpage-1'>
-      <a target="_blank" href="https://crsproject.netlify.app">
+      <a target="_blank" href="https://crsproject.netlify.app" rel="noreferrer">
         <img
           className="d-block"
           src={CRS}
+          alt="IPC webpage"
+          width= "200px"
+          height= "130px"  
+        />
+       </a>
+            
+      </div>
+
+      <div className='webpage-2'>
+      <a target="_blank" href="https://ipc-controls.netlify.app" rel="noreferrer">
+        <img
+          className="d-block"
+          src={IPC}
           alt="CRS webpage"
           width= "200px"
           height= "130px"  
@@ -39,6 +53,8 @@ useEffect (() => {
        </a>
             
       </div>
+
+      
     </div>
     </div>
     </>
